9dac1c585ca40df421628799e051bc2758183d02
Guillaume Lamirand <guillaumelamirand@pingidentity.com>
2025-08-26 16:40:12 +0200

    OPENIG-8693 Apply latest IDM Service configuration

--- config/7.3.0/securebanking/ig/scripts/groovy/RepoApiClient.groovy
+++ config/7.3.0/securebanking/ig/scripts/groovy/RepoApiClient.groovy
@@ -1,4 +1,6 @@
 import groovy.json.JsonOutput
+import static org.forgerock.util.CloseSilentlyAsyncFunction.closeSilently;
+import static org.forgerock.util.promise.NeverThrowsException.neverThrownAsync;
 
 def fapiInteractionId = request.getHeaders().getFirst("x-fapi-interaction-id");
 if(fapiInteractionId == null) fapiInteractionId = "No x-fapi-interaction-id";
@@ -28,36 +30,33 @@ def apiClientId = splitUri[splitUri.length - 1];
 logger.debug(SCRIPT_NAME + "Looking up API Client {}",apiClientId)
 
 apiClientRequest.setUri(routeArgIdmBaseUri + "/openidm/managed/" + routeArgObjApiClient + "/" + apiClientId)
+idmService.send(context, apiClientRequest)
+          .thenAsync(closeSilently(response -> {
+              logger.debug(SCRIPT_NAME + "Back from IDM")
+              def apiClientResponseStatus = apiClientResponse.getStatus();
 
-http.send(apiClientRequest).then(apiClientResponse -> {
-    apiClientRequest.close()
-    logger.debug(SCRIPT_NAME + "Back from IDM")
+              if (apiClientResponseStatus != Status.OK) {
+                  message = "Failed to get API Client details"
+                  logger.error(message)
+                  response.status = apiClientResponseStatus
+                  response.entity = "{ \"error\":\"" + message + "\"}"
+                  return response
+              }
+              return response.getEntity()
+                             .getJsonAsync()
+                             .then(json -> {
+                                 def responseObj = [
+                                         "id"            : json.id,
+                                         "name"          : json.name,
+                                         "officialName"  : json.name,
+                                         "oauth2ClientId": json.oauth2ClientId,
+                                         "logoUri"       : json.logoUri
+                                 ]
 
-    def apiClientResponseStatus = apiClientResponse.getStatus();
+                                 def responseJson = JsonOutput.toJson(responseObj);
+                                 logger.debug(SCRIPT_NAME + "Final JSON " + responseJson)
 
-    if (apiClientResponseStatus != Status.OK) {
-        message = "Failed to get API Client details"
-        logger.error(message)
-        response.status = apiClientResponseStatus
-        response.entity = "{ \"error\":\"" + message + "\"}"
-        return response
-    }
-
-    def apiClientResponseContent = apiClientResponse.getEntity();
-    def apiClientResponseObject = apiClientResponseContent.getJson();
-
-    def responseObj = [
-            "id": apiClientResponseObject.id,
-            "name": apiClientResponseObject.name,
-            "officialName": apiClientResponseObject.name,
-            "oauth2ClientId": apiClientResponseObject.oauth2ClientId,
-            "logoUri": apiClientResponseObject.logoUri
-    ]
-
-    def responseJson = JsonOutput.toJson(responseObj);
-    logger.debug(SCRIPT_NAME + "Final JSON " + responseJson)
-
-    response.entity = responseJson;
-    return response
-
-}).then(response -> { return response })
+                                 response.entity = responseJson;
+                                 return response
+                             });
+          }), neverThrownAsync())
